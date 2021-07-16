5dba0790a298ac489c2dd821fb32bb741d60dfe9
KirillAstafev <gamerstyle.ya.ru@gmail.com>
2021-07-16 19:54:59 +0300

    Ещё раз справлен SQL-запрос для отбора преподавателей (добавлена группировка по id студента (чтобы узнать их количество) и добавлен отбор различных преподавателей, чтобы исключить их повторное появление)

--- src/main/java/com/example/testtask/repositories/LecturerRepository.java
+++ src/main/java/com/example/testtask/repositories/LecturerRepository.java
@@ -11,13 +11,13 @@ public interface LecturerRepository extends JpaRepository<Lecturer, Long> {
     Запрос, извлекающий из БД всех преподавателей, преподающих курс, максимальная нагрузка которых (20 студентов)
     ещё не достигнута. Результат сортируется в порядке возрастания текущей нагрузки на преподавателей
      */
-    @Query(value = "select lecturer.lecturer_id, lecturer_forename, lecturer_patronymic, lecturer_surname " +
+    @Query(value = "select distinct lecturer.lecturer_id, lecturer_forename, lecturer_patronymic, lecturer_surname " +
             "from lecturer inner join lecturers_courses lc on lecturer.lecturer_id = lc.lecturer_id " +
             "inner join course c on lc.course_id = c.course_id " +
             "left join journal j on lecturer.lecturer_id = j.lecturer_id " +
             "where c.course_id=:courseId " +
-            "group by lecturer.lecturer_id, lecturer_forename, lecturer_patronymic, lecturer_surname " +
-            "having count(*) < 20 order by count(*)",
+            "group by student_id, lecturer.lecturer_id, lecturer_forename, lecturer_patronymic, lecturer_surname " +
+            "having count(*) < 20",
             nativeQuery = true)
     List<Lecturer> findAvailableLecturersForCourse(Long courseId);
 }
